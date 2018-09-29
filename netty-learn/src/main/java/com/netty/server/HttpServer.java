9482ebd5e6334af8b697bc6a20378e1ec5d05852
ruirui.qu <ruirui.qu@qunar.com>
2018-09-29 12:21:15 +0800

    add

--- netty-learn/src/main/java/com/netty/server/HttpServer.java
+++ netty-learn/src/main/java/com/netty/server/HttpServer.java
@@ -8,7 +8,7 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
 
 // --------------------- Change Logs----------------------
 // <p>@author ruirui.qu Initial Created at 18/9/27<p>
@@ -21,29 +21,28 @@ public class HttpServer {
     }
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage" + HttpServer.class.getSimpleName() + "port");
-            return;
+        try {
+            int port = 8080;
+            new HttpServer(port).start();
+        } catch (Exception e) {
+            System.out.println("===>" + e);
         }
-        int port = Integer.parseInt(args[0]);
-        new HttpServer(port).start();
     }
 
-    private void start() {
+    public void start() throws Exception {
         ServerBootstrap b = new ServerBootstrap();
         NioEventLoopGroup group = new NioEventLoopGroup();
-
         b.group(group).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
             @Override
-            protected void initChannel(SocketChannel ch) throws Exception {
-                System.out.println("initChannel ch:"+ch);
-                ch.pipeline().addLast("decoder",new HttpRequestDecoder())
-                        .addLast("encoder",new HttpRequestEncoder())
-                        .addLast("aggregator",new HttpObjectAggregator(512*1024))
-                        .addLast("handler",new HttpHandler());
-
+            public void initChannel(SocketChannel ch) throws Exception {
+                System.out.println("initChannel ch:" + ch);
+                ch.pipeline().addLast("decoder", new HttpRequestDecoder())
+                        .addLast("encoder", new HttpResponseEncoder())
+                        .addLast("aggregator", new HttpObjectAggregator(512 * 1024))
+                        .addLast("handler", new HttpHandler());
             }
         }).option(ChannelOption.SO_BACKLOG, 128).childOption(ChannelOption.SO_KEEPALIVE, Boolean.TRUE);
 
+        b.bind(port).sync();
     }
 }
