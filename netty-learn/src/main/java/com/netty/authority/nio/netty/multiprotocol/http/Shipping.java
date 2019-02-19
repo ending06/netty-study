65df322cda6eabcf8f8540458ede175f2d38f6b1
ruirui.qu <ruirui.qu@qunar.com>
2019-02-19 08:56:30 +0800

    add

---
+++ netty-learn/src/main/java/com/netty/authority/nio/netty/multiprotocol/http/Shipping.java
@@ -0,0 +1,18 @@
+package com.netty.authority.nio.netty.multiprotocol.http;
+
+// --------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2019/2/18<p>
+// -------------------------------------------------------
+
+public enum Shipping {
+
+    STANDARD_MALL,
+
+    PRIORITY_MALL,
+
+    INTERNATIONAL_MALL,
+
+    DOMESTIC_EXPRESS,
+
+    INTERNATIONAL_EXPRESS;
+}
