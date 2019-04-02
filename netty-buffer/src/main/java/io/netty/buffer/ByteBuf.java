b3a138ea4299715f1dd13382760704ccca535d79
ruirui.qu <ruirui.qu@qunar.com>
2019-04-02 14:12:46 +0800

    add ByteBuf

--- netty-buffer/src/main/java/io/netty/buffer/ByteBuf.java
+++ netty-buffer/src/main/java/io/netty/buffer/ByteBuf.java
@@ -7,4 +7,16 @@ import io.netty.util.ReferenceCounted;
 // -------------------------------------------------------
 
 public abstract class ByteBuf implements ReferenceCounted, Comparable<ByteBuf> {
+
+    /**
+     * 返回buffer的容量
+     * */
+    public abstract int capacity();
+
+    /**
+     * 调整缓存容量大小
+     * current buffer size > newCapacity size:当前buffer的内容会被截断
+     * current buffer size < newCapacity size:会追加一个buffer，其内容未知，大小为(newCapacity size-current buffer size)大小的buffer，
+     * */
+    public abstract ByteBuf capacity(int newCapacity);
 }
