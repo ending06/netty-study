0f930d15a31ffdc7a4ae9fb2d53d912becbf4a9a
ruirui.qu <ruirui.qu@qunar.com>
2019-04-09 09:17:54 +0800

    add ByteBuf

---
+++ netty-common/src/main/java/io/netty/util/internal/logging/InternalLogLevel.java
@@ -0,0 +1,19 @@
+package io.netty.util.internal.logging;
+
+//--------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2019/4/3<p>
+// 内部logger级别
+//-------------------------------------------------------
+
+public enum InternalLogLevel {
+
+    TRACE,
+
+    DEBUG,
+
+    INFO,
+
+    WARN,
+
+    ERROR;
+}
--- netty-common/src/main/java/io/netty/util/internal/logging/InternalLogger.java
+++ netty-common/src/main/java/io/netty/util/internal/logging/InternalLogger.java
@@ -1,9 +1,9 @@
 package io.netty.util.internal.logging;
 
-//--------------------- Change Logs----------------------
+// --------------------- Change Logs----------------------
 // <p>@author ruirui.qu Initial Created at 2019/4/3<p>
 // 内部使用的logger
-//-------------------------------------------------------
+// -------------------------------------------------------
 
 public interface InternalLogger {
 
@@ -11,4 +11,26 @@ public interface InternalLogger {
      * return the name of this instance
      * */
     String name();
+
+    /**
+     * 开启trace日志
+     * */
+    boolean isTraceEnabled();
+
+    void trace(String name);
+
+    void trace(String format, Object msg);
+
+    void trace(String format, Object... arguments);
+
+    void trace(Throwable t);
+
+    boolean isDebugEnabled();
+
+    void debug(String msg);
+
+    void debug(String format, Object arg);
+
+    void debug(String format, Object argA, Object argsB);
+
 }
